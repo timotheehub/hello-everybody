bee864a113f98f0e83d8aaf89c2515905fe09871
timothee.ledure@gmail.com <timothee.ledure@gmail.com@39d99b22-0ced-cf39-ee74-ab2684ce10d5>
2012-02-09 23:02:11 +0000

    Liste de favoris

--- src/fr/insa/helloeverybody/HelloEverybodyActivity.java
+++ src/fr/insa/helloeverybody/HelloEverybodyActivity.java
@@ -2,7 +2,7 @@ package fr.insa.helloeverybody;
 
 import android.app.TabActivity;
 import android.content.Intent;
-import android.content.res.Resources;
+//import android.content.res.Resources;
 import android.os.Bundle;
 import android.widget.TabHost;
 
@@ -13,7 +13,7 @@ public class HelloEverybodyActivity extends TabActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
 
-        Resources res = getResources(); // Resource object to get Drawables
+        //Resources res = getResources(); // Resource object to get Drawables
         TabHost tabHost = getTabHost();  // The activity TabHost
         TabHost.TabSpec spec;  // Resusable TabSpec for each tab
         Intent intent;  // Reusable Intent for each tab
