ced9b5433a54c1d842ad4d0977f7a07b7b37e5d4
timothee.ledure@gmail.com <timothee.ledure@gmail.com@39d99b22-0ced-cf39-ee74-ab2684ce10d5>
2012-02-13 13:55:11 +0000

    Favoris, Récents, Recommandés et A proximté

--- src/fr/insa/helloeverybody/ContactsActivity.java
+++ src/fr/insa/helloeverybody/ContactsActivity.java
@@ -19,16 +19,20 @@ import android.widget.Toast;
 
 public class ContactsActivity extends Activity {
 	
+	// Listes de contacts (ListView)
 	private ListView favoritesListView;
+	private ListView knownListView;
+	private ListView recommendedListView;
+	private ListView nearMeListView;
 	private ServerInteraction serverInteraction;
 	
-    // Appelï¿½ a la creation
+	
+	
+    // Appelé a la creation
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-        //TextView textview = new TextView(this);
-        //textview.setText("This is the Contacts tab");
         setContentView(R.layout.contacts);
         
         //Get informations from the server
@@ -43,99 +47,278 @@ public class ContactsActivity extends Activity {
         Toast.makeText(ContactsActivity.this, res.toString(), Toast.LENGTH_SHORT).show();
         
         fillFavorites();
+        fillKnown();
+        fillRecommended();
+        fillNearMe();
     }
     
-    // Mï¿½thode qui se dï¿½clenchera lorsque vous appuierez sur le bouton menu du tï¿½lï¿½phone
+    
+    
+    // Méthode qui se déclenchera lorsque vous appuierez sur le bouton menu du téléphone
     public boolean onCreateOptionsMenu(Menu menu) {
  
-        // Crï¿½ation d'un MenuInflater qui va permettre d'instancier un Menu XML en un objet Menu
+        // Cr?ation d'un MenuInflater qui va permettre d'instancier un Menu XML en un objet Menu
         MenuInflater inflater = getMenuInflater();
         
-        // Instanciation du menu XML spï¿½cifier en un objet Menu
+        // Instanciation du menu XML sp?cifier en un objet Menu
         inflater.inflate(R.menu.contacts, menu);
  
         return true;
     }
  
-	// Mï¿½thode qui se dï¿½clenchera au clic sur un item
+    
+    
+	// Méthode qui se déclenchera au clic sur un item
 	public boolean onOptionsItemSelected(MenuItem item) {
-         // On regarde quel item a ï¿½tï¿½ cliquï¿½ grï¿½ce ï¿½ son id et on dï¿½clenche une action
+         // On regarde quel item a ?t? cliqu? gr?ce ? son id et on d?clenche une action
          switch (item.getItemId()) {
-            case R.id.parameters:
-            	// Ouvrir la fenï¿½tre des paramï¿½tres
-               Toast.makeText(ContactsActivity.this, "Paramï¿½tres Contact", Toast.LENGTH_SHORT).show();
+            case R.id.parametres:
+            	// Ouvrir la fen?tre des param?tres
+               Toast.makeText(ContactsActivity.this, "Param?tres Contact", Toast.LENGTH_SHORT).show();
                return true;
-            case R.id.search:
-            	// Ouvrir la fenï¿½tre de recherche
+            case R.id.recherche:
+            	// Ouvrir la fen?tre de recherche
                 Toast.makeText(ContactsActivity.this, "Recherche", Toast.LENGTH_SHORT).show();
                 return true;
-            case R.id.logout:
-            	// Dï¿½connexion et quitter l'application
+            case R.id.deconnexion:
+            	// D?connexion et quitter l'application
                finish();
                return true;
          }
          return false;
 	}
 	
+	
+	
 	// Remplit la liste de favoris
 	private void fillFavorites() {
-		// Rï¿½cupï¿½ration de la listview crï¿½ï¿½e dans le fichier main.xml
-		favoritesListView = (ListView) findViewById(R.id.favorite_list);
+		// R?cup?ration de la listview cr??e dans le fichier main.xml
+		favoritesListView = (ListView) findViewById(R.id.favoritesList);
  
-        // Crï¿½ation de la ArrayList qui nous permettra de remplir la listView
+        // Cr?ation de la ArrayList qui nous permettra de remplir la listView
         ArrayList<HashMap<String, String>> favoritesList = 
         			new ArrayList<HashMap<String, String>>();
  
-        // On dï¿½clare la HashMap qui contiendra les informations pour un item
+        // On d?clare la HashMap qui contiendra les informations pour un item
         HashMap<String, String> favoriteAttributesMap;
         
         // Ajout des favoris
         favoriteAttributesMap = new HashMap<String, String>();
-        favoriteAttributesMap.put("contactPrenom", "Arthur");
-        favoriteAttributesMap.put("contactNom", "M.");
-        favoriteAttributesMap.put("contactImage", 
+        favoriteAttributesMap.put("firstName", "Arthur");
+        favoriteAttributesMap.put("lastName", "M.");
+        favoriteAttributesMap.put("picture", 
         			String.valueOf(R.drawable.default_profile_icon));
         favoritesList.add(favoriteAttributesMap);
         
         favoriteAttributesMap = new HashMap<String, String>();
-        favoriteAttributesMap.put("contactPrenom", "Bob");
-        favoriteAttributesMap.put("contactNom", "L'ï¿½ponge");
-        favoriteAttributesMap.put("contactImage",
+        favoriteAttributesMap.put("firstName", "Bob");
+        favoriteAttributesMap.put("lastName", "L'éponge");
+        favoriteAttributesMap.put("picture",
         			String.valueOf(R.drawable.sponge_bob));
         favoritesList.add(favoriteAttributesMap);
  
         favoriteAttributesMap = new HashMap<String, String>();
-        favoriteAttributesMap.put("contactPrenom", "Patrick");
-        favoriteAttributesMap.put("contactNom", "L'ï¿½toile de mer");
-        favoriteAttributesMap.put("contactImage",
+        favoriteAttributesMap.put("firstName", "Patrick");
+        favoriteAttributesMap.put("lastName", "L'étoile de mer");
+        favoriteAttributesMap.put("picture",
         			String.valueOf(R.drawable.default_profile_icon));
         favoritesList.add(favoriteAttributesMap);
  
         favoriteAttributesMap = new HashMap<String, String>();
-        favoriteAttributesMap.put("contactPrenom", "Timothï¿½e");
-        favoriteAttributesMap.put("contactNom", "L.");
-        favoriteAttributesMap.put("contactImage", 
+        favoriteAttributesMap.put("firstName", "Timothée");
+        favoriteAttributesMap.put("lastName", "L.");
+        favoriteAttributesMap.put("picture", 
         			String.valueOf(R.drawable.default_profile_icon));
         favoritesList.add(favoriteAttributesMap);
  
-        // Crï¿½ation d'un SimpleAdapter qui se chargera de mettre
+        // Cr?ation d'un SimpleAdapter qui se chargera de mettre
         // les favoris de la liste dans la vue contact_item
         SimpleAdapter favoritesAdapter = new SimpleAdapter (this.getBaseContext(),
         		favoritesList, R.layout.contact_item,
-        		new String[] {"contactImage", "contactPrenom", "contactNom"}, 
-        		new int[] {R.id.contactImage, R.id.contactPrenom, R.id.contactNom});
+        		new String[] {"picture", "firstName", "lastName"}, 
+        		new int[] {R.id.picture, R.id.firstName, R.id.lastName});
         favoritesListView.setAdapter(favoritesAdapter);
  
         // Ecoute des clicks sur les items
         favoritesListView.setOnItemClickListener(new OnItemClickListener() {
         	@SuppressWarnings("unchecked")
             public void onItemClick(AdapterView<?> adapter, View view, int position, long id) {
-				// On rï¿½cupï¿½re la HashMap
+				// On r?cup?re la HashMap
         		HashMap<String, String> map = (HashMap<String, String>) favoritesListView.getItemAtPosition(position);
         		
-        		// On affiche le bouton cliquï¿½
+        		// On affiche le bouton cliqu?
         		Toast.makeText(ContactsActivity.this, 
-        				map.get("contactPrenom") + " " + map.get("contactNom"), 
+        				map.get("firstName") + " " + map.get("lastName"), 
+        				Toast.LENGTH_SHORT).show();
+        	}
+         });
+	}
+	
+	
+	
+	// Remplit la liste des personnes à qui on a déjà parlé
+	private void fillKnown() {
+		// Récupération de la listview créée dans le fichier main.xml
+		knownListView = (ListView) findViewById(R.id.knownList);
+ 
+        // Création de la ArrayList qui nous permettra de remplir la listView
+        ArrayList<HashMap<String, String>> knownList = 
+        			new ArrayList<HashMap<String, String>>();
+ 
+        // On déclare la HashMap qui contiendra les informations pour un item
+        HashMap<String, String> knownAttributesMap;
+        
+        // Ajout des connus
+        knownAttributesMap = new HashMap<String, String>();
+        knownAttributesMap.put("firstName", "Julian");
+        knownAttributesMap.put("lastName", "Dos Santos");
+        knownAttributesMap.put("picture", 
+        			String.valueOf(R.drawable.default_profile_icon));
+        knownList.add(knownAttributesMap);
+        
+        knownAttributesMap = new HashMap<String, String>();
+        knownAttributesMap.put("firstName", "Vincent");
+        knownAttributesMap.put("lastName", "B.");
+        knownAttributesMap.put("picture", 
+        			String.valueOf(R.drawable.default_profile_icon));
+        knownList.add(knownAttributesMap);
+ 
+        // Création d'un SimpleAdapter qui se chargera de mettre
+        // les connus de la liste dans la vue contact_item
+        SimpleAdapter knownAdapter = new SimpleAdapter (this.getBaseContext(),
+        		knownList, R.layout.contact_item,
+        		new String[] {"picture", "firstName", "lastName"}, 
+        		new int[] {R.id.picture, R.id.firstName, R.id.lastName});
+        knownListView.setAdapter(knownAdapter);
+ 
+        // Ecoute des clicks sur les items
+        knownListView.setOnItemClickListener(new OnItemClickListener() {
+        	@SuppressWarnings("unchecked")
+            public void onItemClick(AdapterView<?> adapter, View view, int position, long id) {
+				// On récupère la HashMap
+        		HashMap<String, String> map = (HashMap<String, String>) knownListView.getItemAtPosition(position);
+        		
+        		// On affiche le bouton cliqué
+        		Toast.makeText(ContactsActivity.this, 
+        				map.get("firstName") + " " + map.get("lastName"), 
+        				Toast.LENGTH_SHORT).show();
+        	}
+         });
+	}
+	
+	
+	
+	// Remplit la liste de recommandés
+	private void fillRecommended() {
+		// Récupération de la listview créée dans le fichier main.xml
+		recommendedListView = (ListView) findViewById(R.id.recommendedList);
+ 
+        // Création de la ArrayList qui nous permettra de remplir la listView
+        ArrayList<HashMap<String, String>> recommendedList = 
+        			new ArrayList<HashMap<String, String>>();
+ 
+        // On déclare la HashMap qui contiendra les informations pour un item
+        HashMap<String, String> recommendedAttributesMap;
+        
+        // Ajout des recommandés
+        recommendedAttributesMap = new HashMap<String, String>();
+        recommendedAttributesMap.put("firstName", "Li Chen");
+        recommendedAttributesMap.put("lastName", "T.");
+        recommendedAttributesMap.put("picture", 
+        			String.valueOf(R.drawable.default_profile_icon));
+        recommendedList.add(recommendedAttributesMap);
+ 
+        recommendedAttributesMap = new HashMap<String, String>();
+        recommendedAttributesMap.put("firstName", "Loïc");
+        recommendedAttributesMap.put("lastName", "T.");
+        recommendedAttributesMap.put("picture", 
+        			String.valueOf(R.drawable.default_profile_icon));
+        recommendedList.add(recommendedAttributesMap);
+
+        recommendedAttributesMap = new HashMap<String, String>();
+        recommendedAttributesMap.put("firstName", "Raphaël");
+        recommendedAttributesMap.put("lastName", "Corral");
+        recommendedAttributesMap.put("picture", 
+        			String.valueOf(R.drawable.default_profile_icon));
+        recommendedList.add(recommendedAttributesMap);
+        
+        // Création d'un SimpleAdapter qui se chargera de mettre
+        // les recommandés de la liste dans la vue contact_item
+        SimpleAdapter recommendedAdapter = new SimpleAdapter (this.getBaseContext(),
+        		recommendedList, R.layout.contact_item,
+        		new String[] {"picture", "firstName", "lastName"}, 
+        		new int[] {R.id.picture, R.id.firstName, R.id.lastName});
+        recommendedListView.setAdapter(recommendedAdapter);
+ 
+        // Ecoute des clicks sur les items
+        recommendedListView.setOnItemClickListener(new OnItemClickListener() {
+        	@SuppressWarnings("unchecked")
+            public void onItemClick(AdapterView<?> adapter, View view, int position, long id) {
+				// On récupère la HashMap
+        		HashMap<String, String> map = (HashMap<String, String>) recommendedListView.getItemAtPosition(position);
+        		
+        		// On affiche le bouton cliqué
+        		Toast.makeText(ContactsActivity.this, 
+        				map.get("firstName") + " " + map.get("lastName"), 
+        				Toast.LENGTH_SHORT).show();
+        	}
+         });
+	}
+	
+	
+	
+	// Remplit la liste des personnes proches
+	private void fillNearMe() {
+		// Récupération de la listview créée dans le fichier main.xml
+		nearMeListView = (ListView) findViewById(R.id.nearMeList);
+ 
+        // Création de la ArrayList qui nous permettra de remplir la listView
+        ArrayList<HashMap<String, String>> nearMeList = 
+        			new ArrayList<HashMap<String, String>>();
+ 
+        // On déclare la HashMap qui contiendra les informations pour un item
+        HashMap<String, String> nearMeAttributesMap;
+        
+        // Ajout des personnes proches
+        nearMeAttributesMap = new HashMap<String, String>();
+        nearMeAttributesMap.put("firstName", "Flora");
+        nearMeAttributesMap.put("lastName", "B.");
+        nearMeAttributesMap.put("picture",
+        			String.valueOf(R.drawable.default_profile_icon));
+        nearMeList.add(nearMeAttributesMap);
+        
+        nearMeAttributesMap = new HashMap<String, String>();
+        nearMeAttributesMap.put("firstName", "Mathilde");
+        nearMeAttributesMap.put("lastName", "A.");
+        nearMeAttributesMap.put("picture",
+        			String.valueOf(R.drawable.default_profile_icon));
+        nearMeList.add(nearMeAttributesMap);
+
+        nearMeAttributesMap = new HashMap<String, String>();
+        nearMeAttributesMap.put("firstName", "Pikachu");
+        nearMeAttributesMap.put("lastName", "Le pokémon");
+        nearMeAttributesMap.put("picture", 
+        			String.valueOf(R.drawable.default_profile_icon));
+        nearMeList.add(nearMeAttributesMap);
+ 
+        // Création d'un SimpleAdapter qui se chargera de mettre
+        // les personnes proches de la liste dans la vue contact_item
+        SimpleAdapter nearMeAdapter = new SimpleAdapter (this.getBaseContext(),
+        		nearMeList, R.layout.contact_item,
+        		new String[] {"picture", "firstName", "lastName"}, 
+        		new int[] {R.id.picture, R.id.firstName, R.id.lastName});
+        nearMeListView.setAdapter(nearMeAdapter);
+ 
+        // Ecoute des clicks sur les items
+        nearMeListView.setOnItemClickListener(new OnItemClickListener() {
+        	@SuppressWarnings("unchecked")
+            public void onItemClick(AdapterView<?> adapter, View view, int position, long id) {
+				// On récupère la HashMap
+        		HashMap<String, String> map = (HashMap<String, String>) nearMeListView.getItemAtPosition(position);
+        		
+        		// On affiche le bouton cliqué
+        		Toast.makeText(ContactsActivity.this, 
+        				map.get("firstName") + " " + map.get("lastName"), 
         				Toast.LENGTH_SHORT).show();
         	}
          });
