99268072ffb11010aa4514f47a7bfcca9c697093
alex0700 <alex0700@39d99b22-0ced-cf39-ee74-ab2684ce10d5>
2012-02-13 13:48:53 +0000

    Enregistrement serveur OK

--- trunk/src/fr/insa/helloeverybody/ContactsActivity.java
+++ trunk/src/fr/insa/helloeverybody/ContactsActivity.java
@@ -3,6 +3,8 @@ package fr.insa.helloeverybody;
 import java.util.ArrayList;
 import java.util.HashMap;
 
+import fr.insa.helloeverybody.communication.ServerInteraction;
+
 import android.app.Activity;
 import android.os.Bundle;
 import android.view.Menu;
@@ -18,8 +20,9 @@ import android.widget.Toast;
 public class ContactsActivity extends Activity {
 	
 	private ListView favoritesListView;
+	private ServerInteraction serverInteraction;
 	
-    // Appelé a la creation
+    // Appelï¿½ a la creation
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -28,35 +31,46 @@ public class ContactsActivity extends Activity {
         //textview.setText("This is the Contacts tab");
         setContentView(R.layout.contacts);
         
+        //Get informations from the server
+        String serverAdr = "http://10.0.2.2/otsims/ws.php";
+        Profil profil = new Profil();
+        profil.setName("MonNom");
+        profil.setPrenom("Prenom");
+        
+        serverInteraction = new ServerInteraction(this, serverAdr);
+        Boolean res = serverInteraction.register(profil);
+        
+        Toast.makeText(ContactsActivity.this, res.toString(), Toast.LENGTH_SHORT).show();
+        
         fillFavorites();
     }
     
-    // Méthode qui se déclenchera lorsque vous appuierez sur le bouton menu du téléphone
+    // Mï¿½thode qui se dï¿½clenchera lorsque vous appuierez sur le bouton menu du tï¿½lï¿½phone
     public boolean onCreateOptionsMenu(Menu menu) {
  
-        // Création d'un MenuInflater qui va permettre d'instancier un Menu XML en un objet Menu
+        // Crï¿½ation d'un MenuInflater qui va permettre d'instancier un Menu XML en un objet Menu
         MenuInflater inflater = getMenuInflater();
         
-        // Instanciation du menu XML spécifier en un objet Menu
+        // Instanciation du menu XML spï¿½cifier en un objet Menu
         inflater.inflate(R.menu.contacts, menu);
  
         return true;
     }
  
-	// Méthode qui se déclenchera au clic sur un item
+	// Mï¿½thode qui se dï¿½clenchera au clic sur un item
 	public boolean onOptionsItemSelected(MenuItem item) {
-         // On regarde quel item a été cliqué grâce à son id et on déclenche une action
+         // On regarde quel item a ï¿½tï¿½ cliquï¿½ grï¿½ce ï¿½ son id et on dï¿½clenche une action
          switch (item.getItemId()) {
             case R.id.parameters:
-            	// Ouvrir la fenêtre des paramètres
-               Toast.makeText(ContactsActivity.this, "Paramètres Contact", Toast.LENGTH_SHORT).show();
+            	// Ouvrir la fenï¿½tre des paramï¿½tres
+               Toast.makeText(ContactsActivity.this, "Paramï¿½tres Contact", Toast.LENGTH_SHORT).show();
                return true;
             case R.id.search:
-            	// Ouvrir la fenêtre de recherche
+            	// Ouvrir la fenï¿½tre de recherche
                 Toast.makeText(ContactsActivity.this, "Recherche", Toast.LENGTH_SHORT).show();
                 return true;
             case R.id.logout:
-            	// Déconnexion et quitter l'application
+            	// Dï¿½connexion et quitter l'application
                finish();
                return true;
          }
@@ -65,14 +79,14 @@ public class ContactsActivity extends Activity {
 	
 	// Remplit la liste de favoris
 	private void fillFavorites() {
-		// Récupération de la listview créée dans le fichier main.xml
+		// Rï¿½cupï¿½ration de la listview crï¿½ï¿½e dans le fichier main.xml
 		favoritesListView = (ListView) findViewById(R.id.favorite_list);
  
-        // Création de la ArrayList qui nous permettra de remplir la listView
+        // Crï¿½ation de la ArrayList qui nous permettra de remplir la listView
         ArrayList<HashMap<String, String>> favoritesList = 
         			new ArrayList<HashMap<String, String>>();
  
-        // On déclare la HashMap qui contiendra les informations pour un item
+        // On dï¿½clare la HashMap qui contiendra les informations pour un item
         HashMap<String, String> favoriteAttributesMap;
         
         // Ajout des favoris
@@ -85,26 +99,26 @@ public class ContactsActivity extends Activity {
         
         favoriteAttributesMap = new HashMap<String, String>();
         favoriteAttributesMap.put("contactPrenom", "Bob");
-        favoriteAttributesMap.put("contactNom", "L'éponge");
+        favoriteAttributesMap.put("contactNom", "L'ï¿½ponge");
         favoriteAttributesMap.put("contactImage",
         			String.valueOf(R.drawable.sponge_bob));
         favoritesList.add(favoriteAttributesMap);
  
         favoriteAttributesMap = new HashMap<String, String>();
         favoriteAttributesMap.put("contactPrenom", "Patrick");
-        favoriteAttributesMap.put("contactNom", "L'étoile de mer");
+        favoriteAttributesMap.put("contactNom", "L'ï¿½toile de mer");
         favoriteAttributesMap.put("contactImage",
         			String.valueOf(R.drawable.default_profile_icon));
         favoritesList.add(favoriteAttributesMap);
  
         favoriteAttributesMap = new HashMap<String, String>();
-        favoriteAttributesMap.put("contactPrenom", "Timothée");
+        favoriteAttributesMap.put("contactPrenom", "Timothï¿½e");
         favoriteAttributesMap.put("contactNom", "L.");
         favoriteAttributesMap.put("contactImage", 
         			String.valueOf(R.drawable.default_profile_icon));
         favoritesList.add(favoriteAttributesMap);
  
-        // Création d'un SimpleAdapter qui se chargera de mettre
+        // Crï¿½ation d'un SimpleAdapter qui se chargera de mettre
         // les favoris de la liste dans la vue contact_item
         SimpleAdapter favoritesAdapter = new SimpleAdapter (this.getBaseContext(),
         		favoritesList, R.layout.contact_item,
@@ -116,10 +130,10 @@ public class ContactsActivity extends Activity {
         favoritesListView.setOnItemClickListener(new OnItemClickListener() {
         	@SuppressWarnings("unchecked")
             public void onItemClick(AdapterView<?> adapter, View view, int position, long id) {
-				// On récupère la HashMap
+				// On rï¿½cupï¿½re la HashMap
         		HashMap<String, String> map = (HashMap<String, String>) favoritesListView.getItemAtPosition(position);
         		
-        		// On affiche le bouton cliqué
+        		// On affiche le bouton cliquï¿½
         		Toast.makeText(ContactsActivity.this, 
         				map.get("contactPrenom") + " " + map.get("contactNom"), 
         				Toast.LENGTH_SHORT).show();
