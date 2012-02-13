fa0dbc97117899de9f376b62ebbd9c37e0497466
timothee.ledure@gmail.com <timothee.ledure@gmail.com@39d99b22-0ced-cf39-ee74-ab2684ce10d5>
2012-02-13 09:16:26 +0000

    Styles

--- src/fr/insa/helloeverybody/ContactsActivity.java
+++ src/fr/insa/helloeverybody/ContactsActivity.java
@@ -4,7 +4,6 @@ import java.util.ArrayList;
 import java.util.HashMap;
 
 import android.app.Activity;
-import android.app.AlertDialog;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuInflater;
