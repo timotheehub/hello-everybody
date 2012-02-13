394e6bbd002740bfd76338ba105b9d4d523fb626
loic.tersen@gmail.com <loic.tersen@gmail.com@39d99b22-0ced-cf39-ee74-ab2684ce10d5>
2012-02-13 10:41:51 +0000

    Attente de la récupération des contacts à proximité

--- src/fr/insa/helloeverybody/HelloEverybodyActivity.java
+++ src/fr/insa/helloeverybody/HelloEverybodyActivity.java
@@ -1,24 +1,47 @@
 package fr.insa.helloeverybody;
 
+import android.app.ProgressDialog;
 import android.app.TabActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.widget.TabHost;
 
 public class HelloEverybodyActivity extends TabActivity {
+	
+	private ProgressDialog loading;
+	
     /** Called when the activity is first created. */
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.main);
-
-        TabHost tabHost = getTabHost();  // The activity TabHost
+        
+        // Progress dialog and thread for searching contacts
+        loading = ProgressDialog.show(HelloEverybodyActivity.this,
+        		"Chargement...", "Récupération des contacts", true);
+    	new Thread() {
+    		@Override
+    		public void run() {
+    			//TODO: récupération de la liste des contacts à proximité
+    			try {
+					Thread.sleep(2000);
+				} catch (InterruptedException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+    			loading.dismiss();
+		    }
+		}.start();
+        
+		
+		setContentView(R.layout.main);
+    	
+    	TabHost tabHost = getTabHost();  // The activity TabHost
         TabHost.TabSpec spec;  // Resusable TabSpec for each tab
         Intent intent;  // Reusable Intent for each tab
-
+        
         // Create an Intent to launch an Activity for the tab (to be reused)
         intent = new Intent().setClass(this, ProfilActivity.class);
-
+        
         // Initialize a TabSpec for each tab and add it to the TabHost
         spec = tabHost.newTabSpec("profil").setIndicator("Profil")
                       .setContent(intent);
@@ -36,5 +59,7 @@ public class HelloEverybodyActivity extends TabActivity {
         tabHost.addTab(spec);
 
         tabHost.setCurrentTab(1);
+        
     }
+    
 }
