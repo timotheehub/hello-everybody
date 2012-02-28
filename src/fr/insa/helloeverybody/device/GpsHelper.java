c44fe32f0b72184cbb011dd751dd60f4d0392767
loic.tersen@gmail.com <loic.tersen@gmail.com@39d99b22-0ced-cf39-ee74-ab2684ce10d5>
2012-02-28 14:18:48 +0000

    Comparaison entre l'ancienne et la nouvelle position pour decider ou non de la maj

--- src/fr/insa/helloeverybody/device/GpsHelper.java
+++ src/fr/insa/helloeverybody/device/GpsHelper.java
@@ -5,25 +5,52 @@ import android.location.Location;
 import android.location.LocationListener;
 import android.location.LocationManager;
 import android.os.Bundle;
+import android.util.Log;
 
 public class GpsHelper {
+	
+	private static final String LOG_TAG = "Location Changed";
+	
+	// Temps a partir duquel on prend en compte la nouvelle position, en millisecondes
+	private static final int TIME_LIMIT = 1000 * 60 * 1;
+	// Distance a partir duquel on prend en compte la nouvelle position, en metres
+	private static final int DISTANCE_LIMIT = 100;
+	
+	private boolean mFirstTimeLocationChanged;
 
 	private LocationListener mLocationListener;
 	private LocationManager mLocationManager;
 	
 	private Location mCurrentLocation;
+	private Location mLastLocationUpdated;
 	private GpsHelperCallbackInterface mCallback;
+	
 	
 	public GpsHelper(Context context, GpsHelperCallbackInterface callback) {
 		// Acquire a reference to the system Location Manager
 		mLocationManager = (LocationManager) context.getSystemService(Context.LOCATION_SERVICE);
 		mCallback = callback;
+		mFirstTimeLocationChanged = true;
 				
 		// Define a listener that responds to location updates
 		mLocationListener = new LocationListener() {
 			public void onLocationChanged(Location location) {
+				Log.v(LOG_TAG, "location has changed");
+				
+				if(mFirstTimeLocationChanged) {
+					mFirstTimeLocationChanged = false;
+					setLastLocationUpdated(location);
+					mCallback.newLocationFound(location);
+					stopListening();
+				}
+				
+				if(isLocationSignificantlyChanged(location, mLastLocationUpdated)) {
+					Log.v(LOG_TAG, "new location to update");
+					setLastLocationUpdated(location);
+					mCallback.newLocationFound(location);
+				}
+				
 				setLocation(location);
-				mCallback.newLocationFound(location);
 			}
 
 			public void onStatusChanged(String provider, int status, Bundle extras) {}
@@ -41,8 +68,6 @@ public class GpsHelper {
 	
 	public void startListening() {
 		// Register the listener with the Location Manager to receive location updates
-		// Notifications will occur every 10min or every 100m
-		//TODO: pour les tests le temps et la distance entre chaque notification est de 0
 		mLocationManager.requestLocationUpdates(LocationManager.NETWORK_PROVIDER, 0, 0, mLocationListener);
 		mLocationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 0, 0, mLocationListener);
 	}
@@ -52,11 +77,64 @@ public class GpsHelper {
 		mLocationManager.removeUpdates(mLocationListener);
 	}
 	
+	
+	/** Determines if the location has significantly changed to need an update
+	  * @param location  The new Location that you want to evaluate
+	  * @param currentBestLocation  The current Location fix, to which you want to compare the new one
+	  */
+	private boolean isLocationSignificantlyChanged(Location location, Location currentBestLocation) {
+	    if (currentBestLocation == null) {
+	        // A new location is always better than no location
+	        return true;
+	    }
+
+	    // Check whether the new location fix is newer or older
+	    long timeDelta = location.getTime() - currentBestLocation.getTime();
+	    boolean isSignificantlyNewer = timeDelta > TIME_LIMIT;
+	    boolean isSignificantlyOlder = timeDelta < -TIME_LIMIT;
+	    boolean isNewer = timeDelta > 0;
+
+	    // If it's been more than time_limit since the current location, use the new location
+	    // because the user has likely moved
+	    if (isSignificantlyNewer) {
+	        return true;
+	    // If the new location is more than time_limit older, it must be worse
+	    } else if (isSignificantlyOlder) {
+	        return false;
+	    }
+
+	    // Check the distance between these two locations
+	    boolean isSignificantlyDistant = location.distanceTo(currentBestLocation) >= DISTANCE_LIMIT;
+	    
+	    // If the user has moved more than distance_limit, use the new location
+	    if(isNewer && isSignificantlyDistant) {
+	    	return true;
+	    }
+	    
+	    // Check whether the new location fix is more accurate
+	    int accuracyDelta = (int) (location.getAccuracy() - currentBestLocation.getAccuracy());
+	    boolean isMoreAccurate = accuracyDelta < 0;
+	    
+	    // If the new location is more accurate, use it
+	    if (isMoreAccurate) {
+	        return true;
+	    }
+
+	    return false;
+	}
+	
+	
+	private void setLastLocationUpdated(Location location) {
+		this.mLastLocationUpdated = new Location(location);
+	}
+	
+	
 	public Location getLocation() {
 		return mCurrentLocation;
 	}
 
 	public void setLocation(Location location) {
-		this.mCurrentLocation = location;
+		this.mCurrentLocation = new Location(location);
 	}
+	
 }
