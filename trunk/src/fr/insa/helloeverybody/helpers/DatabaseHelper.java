7e5c7def0ddf0d1158b17e7b83f5be6b704d6ab1
arthur.maire@gmail.com <arthur.maire@gmail.com@39d99b22-0ced-cf39-ee74-ab2684ce10d5>
2012-03-12 13:53:26 +0000

    Ajout du password dans la base du profile

--- trunk/src/fr/insa/helloeverybody/helpers/DatabaseHelper.java
+++ trunk/src/fr/insa/helloeverybody/helpers/DatabaseHelper.java
@@ -8,7 +8,7 @@ public class DatabaseHelper extends SQLiteOpenHelper {
 
 	private static final int DB_VERSION = 1;
 	private static final String DB_NAME = "db_profile";
-	private static final String CREATE_TABLE1 = "CREATE TABLE table_profile (jid TEXT NOT NULL, firstName TEXT NOT NULL, lastName TEXT, age INT, relationshipStatus TEXT, sexStatus TEXT)";
+	private static final String CREATE_TABLE1 = "CREATE TABLE table_profile (jid TEXT NOT NULL, firstName TEXT NOT NULL, lastName TEXT, age INT, relationshipStatus TEXT, sexStatus TEXT, password TEXT)";
 	private static final String CREATE_TABLE2 = "CREATE TABLE table_interests (interest TEXT NOT NULL)";
 	private static final String CREATE_TABLE3 = "CREATE TABLE table_contacts (jid TEXT NOT NULL, favorite INT, known INT, recommend INT)";
 	
@@ -24,6 +24,7 @@ public class DatabaseHelper extends SQLiteOpenHelper {
 	public void onCreate(SQLiteDatabase db) {
 		db.execSQL(CREATE_TABLE1);
 		db.execSQL(CREATE_TABLE2);
+		db.execSQL(CREATE_TABLE3);
 		
 	}
 
@@ -31,6 +32,7 @@ public class DatabaseHelper extends SQLiteOpenHelper {
 	public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
 		db.execSQL("DROP TABLE table_interests;");
 		db.execSQL("DROP TABLE table_profile;");
+		db.execSQL("DROP TABLE table_contacts;");
 		onCreate(db);		
 	}
 	
