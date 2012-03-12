83c662bec683fb7fd1673f7743bc307344a536e3
arthur.maire@gmail.com <arthur.maire@gmail.com@39d99b22-0ced-cf39-ee74-ab2684ce10d5>
2012-03-12 08:51:00 +0000

    Test pour la correction du probleme de profil en BD

--- trunk/src/fr/insa/helloeverybody/helpers/DatabaseHelper.java
+++ trunk/src/fr/insa/helloeverybody/helpers/DatabaseHelper.java
@@ -10,6 +10,7 @@ public class DatabaseHelper extends SQLiteOpenHelper {
 	private static final String DB_NAME = "db_profile";
 	private static final String CREATE_TABLE1 = "CREATE TABLE table_profile (ID INTEGER PRIMARY KEY, idProfile LONG, firstName TEXT NOT NULL, lastName TEXT, age INT, relationshipStatus TEXT, sexStatus TEXT)";
 	private static final String CREATE_TABLE2 = "CREATE TABLE table_interests (interest TEXT NOT NULL)";
+	private static final String CREATE_TABLE3 = "CREATE TABLE table_contacts (ID INTEGER NOT NULL, favorite INT, known INT, recommend INT)";
 	
 	public DatabaseHelper(Context context, String nomBdd, SQLiteDatabase.CursorFactory factory, int versionBdd) {
 		super(context, nomBdd, factory, versionBdd);
