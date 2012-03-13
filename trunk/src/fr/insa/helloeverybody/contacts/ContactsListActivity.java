b70bf8c5a13ae22ca90b60c193ed2bbab4b80541
arthur.maire@gmail.com <arthur.maire@gmail.com@39d99b22-0ced-cf39-ee74-ab2684ce10d5>
2012-03-13 10:40:55 +0000

    patch de correction du bug qui empechait l'execution pour les faux contact qui n'était pas en base

--- trunk/src/fr/insa/helloeverybody/contacts/ContactsListActivity.java
+++ trunk/src/fr/insa/helloeverybody/contacts/ContactsListActivity.java
@@ -200,7 +200,13 @@ public class ContactsListActivity extends Activity implements ContactsCallbackIn
         profile = new Profile(null, "Arthur", "M.");
         profile.setJid("abma74331485947");
         Contact contact = db.retrieveContact(profile.getJid());
-        profile.setContact(contact);
+        if (contact != null) {
+        	profile.setContact(contact);
+        } else {
+        	contact = new Contact(profile);
+        	db.insertContact(contact);
+        }
+        
         contactsList.addProfile(profile);
         
         
@@ -213,7 +219,12 @@ public class ContactsListActivity extends Activity implements ContactsCallbackIn
 		bobProfile.getInterestsList().add("Bulles de savon");
 		bobProfile.setJid("bbma74331485568");
 		Contact bobContact = db.retrieveContact(bobProfile.getJid());
-		bobProfile.setContact(bobContact);
+		if (bobContact != null) {
+			bobProfile.setContact(bobContact);
+		} else {
+        	contact = new Contact(bobProfile);
+        	db.insertContact(bobContact);
+        }
 		contactsList.addProfile(bobProfile);
 		
 		
@@ -221,7 +232,12 @@ public class ContactsListActivity extends Activity implements ContactsCallbackIn
 		profile2 = new Profile(null, "Patrick", "L'étoile de mer");
 		profile2.setJid("cbva74331485947");
 		Contact contact2 = db.retrieveContact(profile2.getJid());
-		profile2.setContact(contact2);
+		if (contact2 != null) {
+			profile2.setContact(contact2);
+		} else {
+        	contact2 = new Contact(profile2);
+        	db.insertContact(contact2);
+        }
 		contactsList.addProfile(profile2);
 	
 		
@@ -229,21 +245,36 @@ public class ContactsListActivity extends Activity implements ContactsCallbackIn
 		profile3 = new Profile(null, "Timothée", "L.");
 		profile3.setJid("dbma74331485947");
 		Contact contact3 = db.retrieveContact(profile3.getJid());
-		profile3.setContact(contact3);
+		if (contact3 != null) {
+			profile3.setContact(contact3);
+		} else {
+        	contact3 = new Contact(profile3);
+        	db.insertContact(contact3);
+        }
 		contactsList.addProfile(profile3);
 
 		Profile profile4;
 		profile4 = new Profile(null, "Julian", "Dos Santos");
 		profile4.setJid("ebma74441485947");
 		Contact contact4 = db.retrieveContact(profile4.getJid());
-		profile4.setContact(contact4);
+		if (contact4 != null) {
+			profile4.setContact(contact4);
+		} else {
+        	contact4 = new Contact(profile4);
+        	db.insertContact(contact4);
+        }
 		contactsList.addProfile(profile4);
 		
 		Profile profile5;
 		profile5 = new Profile(null, "Vincent", "B.");
 		profile5.setJid("fbma74551485947");
 		Contact contact5 = db.retrieveContact(profile5.getJid());
-		profile5.setContact(contact5);
+		if (contact5 != null) {
+			profile5.setContact(contact5);
+		} else {
+        	contact5 = new Contact(profile5);
+        	db.insertContact(contact5);
+        }
 		contactsList.addProfile(profile5);
 		
 		// Recommandes
@@ -253,7 +284,12 @@ public class ContactsListActivity extends Activity implements ContactsCallbackIn
 		profile6 = new Profile(null, "Li Chen", "T.");
 		profile6.setJid("gbma74661485947");
 		Contact contact6 = db.retrieveContact(profile6.getJid());
-		profile6.setContact(contact6);
+		if (contact6 != null) {
+			profile6.setContact(contact6);
+		} else {
+        	contact6 = new Contact(profile6);
+        	db.insertContact(contact6);
+        }
 		contactsList.addProfile(profile6);
 		
 		
@@ -261,7 +297,12 @@ public class ContactsListActivity extends Activity implements ContactsCallbackIn
 		profile7 = new Profile(null, "Loïc", "T.");
 		profile7.setJid("hbma74771485947");
 		Contact contact7 = db.retrieveContact(profile7.getJid());
-		profile7.setContact(contact7);
+		if (contact7 != null) {
+			profile7.setContact(contact7);
+		} else {
+        	contact7 = new Contact(profile7);
+        	db.insertContact(contact7);
+        }
 		contactsList.addProfile(profile7);
 		
 		
@@ -269,7 +310,12 @@ public class ContactsListActivity extends Activity implements ContactsCallbackIn
 		profile8 = new Profile(null, "Rafael", "Corral");
 		profile8.setJid("jbma74881485947");
 		Contact contact8 = db.retrieveContact(profile8.getJid());
-		profile8.setContact(contact8);
+		if (contact8 != null) {
+			profile8.setContact(contact8);
+		} else {
+        	contact8 = new Contact(profile8);
+        	db.insertContact(contact8);
+        }
 		contactsList.addProfile(profile8);
 		
 		db.close();
